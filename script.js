899d1164406b8d0d072bcd7c2c143bb5b5d23631
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-14 15:56:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,31 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	   let ans = [];
+    let temp = [];
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        // Check if adding the current element exceeds the limit
+        if (sum + arr[i] <= n) {
+            // If it does not exceed, add to temp and update sum
+            temp.push(arr[i]);
+            sum += arr[i];
+        } else {
+            // If it exceeds or equals, push the current temp to ans
+            ans.push(temp);
+            // Start a new subarray with the current element
+            temp = [arr[i]];
+            sum = arr[i]; // Reset sum to the current element
+        }
+    }
+
+    // If there are remaining elements in temp, add them to ans
+    if (temp.length > 0) {
+        ans.push(temp);
+    }
+
+    return ans;
 };
 
 const n = prompt("Enter n: ");
